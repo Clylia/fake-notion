f23319aad9361a0373dab00ac43ee2ea95978c71
startdusk <522383345@qq.com>
2022-01-18 22:56:19 +0800

    add(*): add gen proto skip

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 Usage:
 server
+
 ```
 $ cd /fake-notion/server
 ```
+
 generator rsa file.
 
 ```
 $ make init
 ```
+
 run
+
 ```
 $ make runall
 ```
+
+TODO
+
+1.Verify the correctness of the email address before registering a user.
--- server/genProto.sh
+++ server/genProto.sh
@@ -8,9 +8,15 @@ function genProto {
 
     protoc -I $PROTO_PATH --go_out $GO_OUT_PATH --go_opt paths=source_relative --go-grpc_out $GO_OUT_PATH --go-grpc_opt paths=source_relative ${DOMAIN}.proto
 
+    if [ $SKIP_GATEWAY ]; then
+        # No external exposure required
+        return
+    fi
+
     protoc -I $PROTO_PATH --grpc-gateway_out $GO_OUT_PATH --grpc-gateway_opt paths=source_relative --grpc-gateway_opt grpc_api_configuration=$PROTO_PATH/${DOMAIN}.yaml ${DOMAIN}.proto
 }
 
 genProto auth
 genProto account
 genProto page
+genProto blob 1
